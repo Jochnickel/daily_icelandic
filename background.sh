538d76207214e7e32a7b9c76355b68e78107aed0
Your Name <you@example.com>
2020-03-10 04:32:29 +0000

    1

--- background.sh
+++ background.sh
@@ -1,1 +1,2 @@
+cd "$(dirname "$0")"
 while true; do python3 main.py; sleep 5; done
--- cronjob.sh
+++ cronjob.sh
@@ -1,2 +1,2 @@
 cd "$(dirname "$0")"
-timeout 60 ./background.sh
+timeout 60 ./background.sh & timeout 60 python3 ../telegram_bot_lua/main.py
